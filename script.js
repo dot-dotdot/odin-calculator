116115543d9da840966bf6dc2cdb40902280df7d
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-08-06 22:30:38 +0700

    Add basic arithmetic functions

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 "use strict";
+
+function add(a, b) {
+    return a + b;
+}
+
+function substract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    if (b !== 0) {
+        return a / b;
+    }
+}
