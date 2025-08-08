30cbb5e4a4863b755c729b89eadd57341199d7b1
dot-dotdot <175088302+dot-dotdot@users.noreply.github.com>
2025-08-08 19:55:31 +0700

    Implement basic evaluation
    Everything is very bad. Right now the calculator only works if
    you use equals after entering two operands and reset after getting
    the result.

--- script.js
+++ script.js
@@ -1,5 +1,62 @@
 "use strict";
 
+let firstOperand = 0;
+let secondOperand = 0;
+let result = 0;
+let operandString = "";
+let operatorString = "";
+let waitingForNextOperand = false;
+
+const exprDisplay = document.querySelector(".expr-display");
+const resultDisplay = document.querySelector(".result-display");
+const numberButtons = document.querySelectorAll(".number");
+const operatorButtons = document.querySelectorAll(".operator");
+const clearButton = document.querySelector("#clear");
+const equalsButton = document.querySelector("#equals");
+
+exprDisplay.value = "";
+resultDisplay.value = "";
+
+numberButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        operandString += button.textContent;
+        resultDisplay.value = operandString;
+    });
+});
+
+operatorButtons.forEach(button => {
+    button.addEventListener("click", () => {
+        if (!waitingForNextOperand) {
+            waitingForNextOperand = true;
+            firstOperand = +operandString;
+        } else {
+            secondOperand = +operandString;
+            result = calculate(firstOperand, secondOperand);
+            resultDisplay.value = result;
+            firstOperand = result;
+        }
+
+        operatorString = button.textContent;
+        operandString = "";
+    });
+});
+
+equalsButton.addEventListener("click", () => {
+    waitingForNextOperand = false;
+    secondOperand = +operandString;
+    result = calculate(firstOperand, secondOperand);
+    resultDisplay.value = result;
+})
+
+clearButton.addEventListener("click", () => reset());
+
+function calculate(a, b) {
+    if (operatorString === "+") return add(a, b);        
+    if (operatorString === "-") return substract(a, b);
+    if (operatorString === "*") return multiply(a, b);
+    if (operatorString === "/") return divide(a, b);
+}
+
 function add(a, b) {
     return a + b;
 }
@@ -16,4 +73,18 @@ function divide(a, b) {
     if (b !== 0) {
         return a / b;
     }
+}
+
+function clear() {
+    firstOperand = 0;
+    secondOperand = 0;
+    operandString = "";
+    operatorString = "";
+}
+
+function reset() {
+    clear();
+    exprDisplay.value = "";
+    resultDisplay.value = "";
+    waitingForNextOperand = false;
 }
